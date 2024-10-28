9d86545e6a151ac1876f66c2a64203b0625b1aa2
Cristian Primera <cristian.primera789@pascualbravo.edu.co>
2024-10-27 20:21:41 -0500

    Iniciar modelo

---
+++ Taller/Taller/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Biblioteca1
+    {
+        //Atributos de la clase biblioteca
+        public int Identificador;
+        public string Titulo_material;
+        public DateTime Fecha_registro;
+        public int Cantidad_registrada;
+        public int Cantidad_actual;
+
+        //Métodos
+
+        public void Prestamo()
+        {
+
+        }
+    }
+
+    public class Persona
+    {
+        public int Id;
+        public string Nombre;
+        public string Rol;
+
+    }
+}
---
+++ Taller/Taller/Persona.cs
@@ -0,0 +1,14 @@
+﻿namespace Modelo
+{
+    public partial class Biblioteca
+    {
+        public class Persona
+        {
+            public int Id;
+            public string Nombre;
+            public string Rol;
+
+
+        }
+    }
+}
