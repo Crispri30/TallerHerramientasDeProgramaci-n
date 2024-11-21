e4b661e59089a2d77d7840b271c8866342a1c419
Cristian Primera <cristian.primera789@pascualbravo.edu.co>
2024-11-20 19:15:32 -0500

    Proyecto terminado

--- Taller/Taller/Form1.Designer.cs
+++ Taller/Taller/Form1.Designer.cs
@@ -31,6 +31,11 @@
             this.ButtonGestionUsuarios = new System.Windows.Forms.Button();
             this.ButtonRegistrarMaterial = new System.Windows.Forms.Button();
             this.ButtonRegistrarPrestamo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ButtonGestionUsuarios
@@ -45,9 +50,9 @@
             // 
             // ButtonRegistrarMaterial
             // 
-            this.ButtonRegistrarMaterial.Location = new System.Drawing.Point(324, 177);
+            this.ButtonRegistrarMaterial.Location = new System.Drawing.Point(330, 182);
             this.ButtonRegistrarMaterial.Name = "ButtonRegistrarMaterial";
-            this.ButtonRegistrarMaterial.Size = new System.Drawing.Size(127, 51);
+            this.ButtonRegistrarMaterial.Size = new System.Drawing.Size(103, 42);
             this.ButtonRegistrarMaterial.TabIndex = 2;
             this.ButtonRegistrarMaterial.Text = "Registrar material";
             this.ButtonRegistrarMaterial.UseVisualStyleBackColor = true;
@@ -55,7 +60,7 @@
             // 
             // ButtonRegistrarPrestamo
             // 
-            this.ButtonRegistrarPrestamo.Location = new System.Drawing.Point(533, 186);
+            this.ButtonRegistrarPrestamo.Location = new System.Drawing.Point(565, 182);
             this.ButtonRegistrarPrestamo.Name = "ButtonRegistrarPrestamo";
             this.ButtonRegistrarPrestamo.Size = new System.Drawing.Size(108, 33);
             this.ButtonRegistrarPrestamo.TabIndex = 4;
@@ -63,17 +68,68 @@
             this.ButtonRegistrarPrestamo.UseVisualStyleBackColor = true;
             this.ButtonRegistrarPrestamo.Click += new System.EventHandler(this.ButtonRegistrarPrestamo_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bienvenido a gestión de biblioteca";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(327, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Por favor seleccione:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Para agregar o eliminar usuarios";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(327, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Para agregar material";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(485, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(265, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Para solicitar préstamo, devolución o adicionar material";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ButtonRegistrarPrestamo);
             this.Controls.Add(this.ButtonRegistrarMaterial);
             this.Controls.Add(this.ButtonGestionUsuarios);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +138,11 @@
         private System.Windows.Forms.Button ButtonGestionUsuarios;
         private System.Windows.Forms.Button ButtonRegistrarMaterial;
         private System.Windows.Forms.Button ButtonRegistrarPrestamo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
